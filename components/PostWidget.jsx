f57efbb8f8df6380c3a64e21110773a959a8f6c8
Sohel <tosohel22@gmail.com>
2022-10-20 18:29:47 +0600

    second commit

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    console.log(relatedPosts);
+    if (slug) {
+      getSimilarPosts(category, slug)
+        .then((result)=> setRelatedPosts(result))
+    } else { 
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [])
+  
   return (
     <div>PostWidget</div>
   )
