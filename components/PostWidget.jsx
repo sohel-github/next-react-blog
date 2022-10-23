4d8d3db9a602e7927f9c62a1ef6494845abba5ff
Sohel <tosohel22@gmail.com>
2022-10-23 15:55:11 +0600

    third commit

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -11,16 +11,42 @@ const PostWidget = ({ categories, slug }) => {
   useEffect(() => {
     console.log(relatedPosts);
     if (slug) {
-      getSimilarPosts(category, slug)
-        .then((result)=> setRelatedPosts(result))
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
     } else { 
       getRecentPosts().then((result) => setRelatedPosts(result));
     }
-  }, [])
+  }, [slug])
   
   return (
-    <div>PostWidget</div>
-  )
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Post" : "Recent Post"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 text-xs">
+              {moment(post.createdAt).format("DD-MM-YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-xs">
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default PostWidget
